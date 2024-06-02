950bc5b97ea17cb06992964be949796cdcc6be81
zeroco0l <zeroco0l@inbox.ru>
2024-06-02 18:41:10 +0200

    Added UpdateTestFailed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import ru.netology.data.Post
 import ru.netology.data.objects.Geo
 import ru.netology.data.objects.Like
 import ru.netology.data.objects.PostType
+import ru.netology.service.WallService
 
 
 fun main() {
--- src/test/kotlin/service/WallServiceTest.kt
+++ src/test/kotlin/service/WallServiceTest.kt
@@ -18,7 +18,7 @@ class WallServiceTest {
 
 
     @Test
-    fun updateExisting() {
+    fun updateExistingPassed() {
         val service = WallService()
         service.add(Post(ownerId = 111, fromId = 111, createdBy = 111, text = "Text"))
         service.add(Post(ownerId = 111, fromId = 111, createdBy = 111))
@@ -27,6 +27,17 @@ class WallServiceTest {
         val result = service.update(update)
 
         assertTrue(result)
+    }
+
+    @Test
+    fun updateExistingFailed() {
+        val service = WallService()
+        service.add(Post(ownerId = 111, fromId = 111, createdBy = 111, text = "Text"))
+        service.add(Post(ownerId = 111, fromId = 111, createdBy = 111))
+        service.add(Post(ownerId = 111, fromId = 111, createdBy = 111, text = "Message"))
+        val update = Post(4, 111, 111, 111, text = "Updated Text")
+        val result = service.update(update)
+     assertFalse(result)
     }
 
 
